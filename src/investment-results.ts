542cb9fb8308227c7ecf725311daa7b59b614f54
Designer <Designer@DESKTOP-NG94IPO>
2026-03-18 16:10:37 +0300

    feat: user-input.component

--- src/investment-results.ts
+++ src/investment-results.ts
@@ -1,7 +1,3 @@
-// Use the below code as a help
-// e.g., integrate it into a service or component
-// You may need to tweak it, depending on where and how you use it
-
 function calculateInvestmentResults() {
   const annualData = [];
   let investmentValue = initialInvestment;
