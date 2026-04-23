c0374ff550718d8f4822a9ea6c1c0621c32e0629
Designer <Designer@DESKTOP-NG94IPO>
2026-04-23 15:28:44 +0300

    feat:Investment-Calculator

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,18 +1,14 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultsData;
-  }
+  results = computed(() => this.investmentService.resultsData());
 }
--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import type { InvestmentInput } from './investment-input.model';
 
 @Injectable({ providedIn: 'root' })
 export class InvestmentService {
-  resultsData?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
+  resultsData = signal<
+    | {
+        year: number;
+        interest: number;
+        valueEndOfYear: number;
+        annualInvestment: number;
+        totalInterest: number;
+        totalAmountInvested: number;
+      }[]
+    | undefined
+  >(undefined);
 
   CalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
@@ -34,7 +37,6 @@ export class InvestmentService {
       });
     }
 
-    //this.resultsData.set(annualData);
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
